a7b55a030c2b6615b041892812ec234b158605b8
Lucas <lucasnrgaard@gmail.com>
2025-05-17 23:33:25 +0200

    refactor: change UnicodeVersionFile type to interface and update schema definition

--- src/routes/v1_unicode-files.schemas.ts
+++ src/routes/v1_unicode-files.schemas.ts
@@ -10,12 +10,29 @@ export const BaseUnicodeVersionFileSchema = z.object({
   }),
 });
 
-export type UnicodeVersionFile = z.infer<typeof BaseUnicodeVersionFileSchema> & {
+export interface UnicodeVersionFile {
+  name: string;
+  path: string;
   children?: UnicodeVersionFile[];
-};
+}
 
-export const UnicodeVersionFileSchema: z.ZodType<UnicodeVersionFile> = BaseUnicodeVersionFileSchema.extend({
-  children: z.lazy(() => UnicodeVersionFileSchema.array()).optional().openapi({
-    description: "The children of the directory, if it is a directory.",
+export const UnicodeVersionFileSchema: z.ZodType<UnicodeVersionFile> = z.object({
+  name: z.string().openapi({
+    description: "The name of the file or directory.",
   }),
-});
+
+  path: z.string().openapi({
+    description: "The path to the file or directory.",
+  }),
+
+  children: z
+    .array(z.lazy(() => UnicodeVersionFileSchema))
+    .optional()
+    .openapi({
+      description: "The children of the directory, if it is a directory.",
+      type: "array",
+      items: {
+        $ref: "#/components/schemas/UnicodeVersionFile",
+      },
+    }),
+}).openapi("UnicodeVersionFile");
